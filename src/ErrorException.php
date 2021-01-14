9e660ebb398633c04a39f407e62c899379ba4494
Paul Water <p.r.water@setbased.nl>
2021-01-14 07:19:14 +0100

    Drop support for PHP 7.3 and PHP 8.x ready.

--- src/ErrorException.php
+++ src/ErrorException.php
@@ -8,23 +8,24 @@ namespace SetBased\Exception;
  */
 class ErrorException extends \ErrorException implements NamedException
 {
-  protected static $ourNames = [E_COMPILE_ERROR     => 'PHP Compile Error',
-                                E_COMPILE_WARNING   => 'PHP Compile Warning',
-                                E_CORE_ERROR        => 'PHP Core Error',
-                                E_CORE_WARNING      => 'PHP Core Warning',
-                                E_DEPRECATED        => 'PHP Deprecated Warning',
-                                E_ERROR             => 'PHP Fatal Error',
-                                E_NOTICE            => 'PHP Notice',
-                                E_PARSE             => 'PHP Parse Error',
-                                E_RECOVERABLE_ERROR => 'PHP Recoverable Error',
-                                E_STRICT            => 'PHP Strict Warning',
-                                E_USER_DEPRECATED   => 'PHP User Deprecated Warning',
-                                E_USER_ERROR        => 'PHP User Error',
-                                E_USER_NOTICE       => 'PHP User Notice',
-                                E_USER_WARNING      => 'PHP User Warning',
-                                E_WARNING           => 'PHP Warning'];
+  protected static array $ourNames = [E_COMPILE_ERROR     => 'PHP Compile Error',
+                                      E_COMPILE_WARNING   => 'PHP Compile Warning',
+                                      E_CORE_ERROR        => 'PHP Core Error',
+                                      E_CORE_WARNING      => 'PHP Core Warning',
+                                      E_DEPRECATED        => 'PHP Deprecated Warning',
+                                      E_ERROR             => 'PHP Fatal Error',
+                                      E_NOTICE            => 'PHP Notice',
+                                      E_PARSE             => 'PHP Parse Error',
+                                      E_RECOVERABLE_ERROR => 'PHP Recoverable Error',
+                                      E_STRICT            => 'PHP Strict Warning',
+                                      E_USER_DEPRECATED   => 'PHP User Deprecated Warning',
+                                      E_USER_ERROR        => 'PHP User Error',
+                                      E_USER_NOTICE       => 'PHP User Notice',
+                                      E_USER_WARNING      => 'PHP User Warning',
+                                      E_WARNING           => 'PHP Warning'];
 
   //--------------------------------------------------------------------------------------------------------------------
+
   /**
    * {@inheritdoc}
    *
